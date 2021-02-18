d039a8cc353c5da88ff08f68c896948202cceba6
Przemek Nosek <przemo99.10@gmail.com>
2021-02-18 13:06:24 +0100

    dodanie solution3 z modulo

--- src/Solution.java
+++ src/Solution.java
@@ -4,9 +4,9 @@ public class Solution {
     public static void main(String[] args) {
         var solution = new Solution();
         int X = 10;
-        int Y = 100;
+        int Y = 101;
         int D = 30;
-        System.out.println(solution.solution(X,Y,D));
+        System.out.println(solution.solution3(X,Y,D));
     }
 
 
@@ -22,5 +22,13 @@ public class Solution {
         if (X == Y)
             return 0;
         return (int)ceil((Y - X) / (double)D);
+    }
+
+    public int solution3(int X, int Y, int D) {
+        int distance = Y - X;
+        int steps = distance / D;
+        if (distance % D > 0)
+            steps++;
+        return steps;
     }
 }
