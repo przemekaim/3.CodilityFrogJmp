ebffcad0b8c529714c7e0fca35679c28dc9689ab
Przemek Nosek <przemo99.10@gmail.com>
2021-02-18 12:35:18 +0100

    najs

---
+++ src/Solution.java
@@ -0,0 +1,20 @@
+import static java.lang.Math.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        var solution = new Solution();
+        int X = 10;
+        int Y = 100;
+        int D = 30;
+        System.out.println(solution.solution(X,Y,D));
+    }
+
+
+    public int solution(int X, int Y, int D) {
+        if (X == Y)
+            return 0;
+        int absDistance = Y - X;
+        double steps = (double)absDistance / (double)D;
+        return (int)ceil(steps);
+    }
+}
